b2333c11f17884e5edb735424409130a9ff3645b
bahe@strw.leidenuniv.nl <bahe@strw.leidenuniv.nl>
2023-06-20 17:22:27 +0200

    Updated build_cluster_catalogue.py

--- build_cluster_catalogue.py
+++ build_cluster_catalogue.py
@@ -5,16 +5,12 @@ Started 22 Jul 2019
 """
 
 import numpy as np
-import yb_utils as yb
-import sim_tools as st
-import hydrangea_tools as ht
+import hydrangea as hy
+import hydrangea.hdf5 as hd
 from pdb import set_trace
 import os
 
-outloc = ('/virgo/scratch/ybahe/HYDRANGEA/RESULTS/ICL/' 
-          'cluster_catalogue_z0_22Jul19_merge1p10_wSFR.hdf5')
-
-basedir = '/virgo/simulations/Hydrangea/10r200/' 
+outloc = ('cluster_catalogue_z0_20Jun23_merge1p10_wSFR.hdf5')
 
 isnap = 29    # Snapshot in which clusters are selected
 
@@ -32,179 +28,146 @@ full_sfr_30kpc = np.copy(full_m200)
 
 full_relaxTime = np.copy(full_m200)
 
-
 for isim in range(30):
+    if isim in [10, 17, 19, 20, 23, 26, 27]:
+        continue
+    print(f"Processing simulation {isim}...")
     
-    rundir = basedir + 'CE-{:d}/HYDRO/' .format(isim)
-    if not os.path.isdir(rundir): continue
+    sim = hy.Simulation(isim)
+    subdir = sim.get_subfind_file(isnap)
+    snapdir = sim.get_snapshot_file(isnap)
 
-    subdir, snapdir = st.form_files(rundir, isnap, 'sub snap')
+    hldir = sim.high_level_dir
+    cantor_tableloc = hldir + 'Cantor/GalaxyTables.hdf5'
+    cantor_file = hldir + f'Cantor/Cantor_{isnap:03d}.hdf5'
+    cantor_id_file = hldir + f'Cantor/Cantor_{isnap:03d}_IDs.hdf5'
 
-    hldir = rundir + 'highlev/'
-    fgtloc = hldir + 'FullGalaxyTables.hdf5'
-    cantorloc = hldir + 'CantorCatalogue.hdf5'
-    posloc = hldir + 'GalaxyPositionsSnap.hdf5'
-    spiderloc = hldir + 'SpiderwebTables.hdf5'
-
-    m200_all = yb.read_hdf5(fgtloc, 'M200')
+    print("   ... reading data...")
+    m200_all = hy.hdf5.read_data(sim.fgt_loc, 'M200')
     m200 = m200_all[:, isnap]
-    r200 = yb.read_hdf5(fgtloc, 'R200')[:, isnap]
+    r200 = hy.hdf5.read_data(sim.fgt_loc, 'R200')[:, isnap]
 
-    mergelist = yb.read_hdf5(spiderloc, 'MergeList')
-
-    cenGal_all = yb.read_hdf5(cantorloc, 'CenGalExtended')
-    #cenGal_all = yb.read_hdf5(fgtloc, 'CenGal')
-
+    mergelist = hy.hdf5.read_data(sim.spider_loc, 'MergeList')
+    cenGal_all = hy.hdf5.read_data(cantor_tableloc, 'CentralGalaxy')
     cenGal = cenGal_all[:, isnap]
 
-    contFlag = yb.read_hdf5(fgtloc, 'Full/ContFlag')[:, 1]
-    mStar_sf = yb.read_hdf5(fgtloc, 'Mstar')[:, isnap]
-    mType_ca = yb.read_hdf5(cantorloc, 'Snapshot_{:03d}/Subhalo/MassType'
-                            .format(isnap))
-    cantor_shi = yb.read_hdf5(cantorloc, 'SubhaloIndex')[:, isnap]
-    sf_shi = yb.read_hdf5(fgtloc, 'SHI')[:, isnap]
+    contFlag = hy.hdf5.read_data(sim.fgt_loc, 'Full/ContFlag')[:, 1]
+    mStar_sf = hy.hdf5.read_data(sim.fgt_loc, 'Mstar')[:, isnap]
+    mType_ca = hy.hdf5.read_data(cantor_file, 'Subhalo/MassType') * 1e10
+    pos_ca = hy.hdf5.read_data(cantor_file, 'Subhalo/CentreOfPotential')
+    
+    cantor_shi = hy.hdf5.read_data(cantor_tableloc, 'SubhaloIndex')[:, isnap]
+    sf_shi = hy.hdf5.read_data(sim.fgt_loc, 'SHI')[:, isnap]
 
-    pos_gal_all = yb.read_hdf5(posloc, 'Centre')[:, isnap, :]
+    pos_gal_all = hy.hdf5.read_data(sim.gps_loc, 'Centre')[:, isnap, :]
 
     ngal = len(m200)
 
     # Select galaxies that qualify as 'cluster centrals':
-    ind_cl = np.nonzero((m200 > 14.0) & (cenGal == np.arange(ngal)) &
-                        (contFlag == 0))[0]
-
+    ind_cl = np.nonzero(
+        (m200 > 14.0) & (cenGal == np.arange(ngal)) & (contFlag == 0))[0]
     ncl = len(ind_cl)
-    print("There are {:d} clusters in simulation {:d}..." 
-          .format(ncl, isim))
+    print(f"There are {ncl} clusters in simulation {isim}...")
     if ncl == 0: continue
 
-    aexp = st.snap_age(snapdir, type = 'aexp')
-    conv_astro_pos = aexp/0.6777
+    ref_ids_cantor = hd.read_data(cantor_id_file, "IDs")
+    ref_off_cantor = hd.read_data(cantor_file, 'Subhalo/OffsetType')
+    ref_end_cantor = hd.read_data(
+        cantor_file, 'Subhalo/Extra/OffsetTypeApertures')
+    ref_eid_cantor = hd.read_data(cantor_file, 'Subhalo/Extra/ExtraIDs')
 
-    ref_ids_cantor = yb.read_hdf5(cantorloc, 
-                                  'Snapshot_{:03d}/IDs' .format(isnap))
-    ref_off_cantor = yb.read_hdf5(
-        cantorloc, 'Snapshot_{:03d}/Subhalo/OffsetType' .format(isnap))
-    ref_end_cantor = yb.read_hdf5(
-        cantorloc, 'Snapshot_{:03d}/Subhalo/Extra/OffsetTypeApertures'
-        .format(isnap))
-    ref_eid_cantor = yb.read_hdf5(
-        cantorloc, 'Snapshot_{:03d}/Subhalo/Extra/ExtraIDs' .format(isnap))
+    ref_ids_subfind = hy.SplitFile(subdir, 'IDs').ParticleID
+    sub = hy.SplitFile(subdir, 'Subhalo')
+    ref_off_subfind = sub.SubOffset
+    ref_len_subfind = sub.SubLength
 
-    ref_ids_subfind = st.eagleread(subdir, 'IDs/ParticleID', astro = False)
-    ref_off_subfind = st.eagleread(subdir, 'Subhalo/SubOffset', astro = False)
-    ref_len_subfind = st.eagleread(subdir, 'Subhalo/SubLength', astro = False)
-
-    snap_ages = ht.snap_times(conv = 'age')
-    snap_lbt = snap_ages[isnap] - snap_ages
+    snap_lbt = hy.snep_times(time_type='lbt', snep_list='allsnaps')
 
     for iicl, icl in enumerate(ind_cl):
-        
+
+        print(f"   ... processing galaxy {icl}...")
         curr_cl += 1
-
         r200_cl = r200[icl]
-        r200_cl_sim = r200_cl/conv_astro_pos
+        cshi = cantor_shi[cenGal[icl]]
+        cl_pos = pos_ca[cshi, :]
+        stars = hy.ReadRegion(snapdir, 4, cl_pos, r200_cl, exact=True)
         
-        cl_pos = pos_gal_all[icl, :]
-        cl_pos_sim = cl_pos/conv_astro_pos
-
-        readReg = ht.ReadRegion(snapdir, 4, [*cl_pos_sim, r200_cl_sim])
-        
-        spos = readReg.read_data("Coordinates", astro = True)
-        smass = readReg.read_data("Mass", astro = True)
-        sid = readReg.read_data("ParticleIDs", astro = False)
-
-        srad_full = np.linalg.norm(spos - cl_pos[None, :], axis = 1)
-        ind_in_sphere = np.nonzero((srad_full >= 0.00) 
-                                   & (srad_full <= r200_cl))[0]
-        
-        srad = srad_full[ind_in_sphere]
-        smass = smass[ind_in_sphere]
-        sid = sid[ind_in_sphere]
-
         full_sim[curr_cl] = isim
         full_gal[curr_cl] = icl
-        full_mstar_all[curr_cl] = np.sum(smass)
+        full_mstar_all[curr_cl] = np.sum(stars.Mass)
         full_m200[curr_cl] = m200[icl]
         full_r200[curr_cl] = r200_cl
 
+        sid = stars.ParticleIDs
+        
         # Match to Cantor particles
-        ref_off = ref_off_cantor[cantor_shi[icl], 4]
-        ref_end = ref_off_cantor[cantor_shi[icl], 5]
+        ref_off = ref_off_cantor[cshi, 4]
+        ref_end = ref_off_cantor[cshi, 5]
         ref_ids = ref_ids_cantor[ref_off : ref_end]
         
-        gate = st.Gate(sid, ref_ids)
-        ref_index = gate.in2()
-        ind_in_ca = np.nonzero(ref_index >= 0)[0]
-
-        full_mstar_ca[curr_cl] = np.sum(smass[ind_in_ca])
+        ref_index, ind_matched = hy.crossref.find_id_indices(
+            stars.ParticleIDs, ref_ids)
+        full_mstar_ca[curr_cl] = np.sum(stars.Mass[ind_matched])
         
         # Match to Subfind particles
         ref_off = ref_off_subfind[sf_shi[icl]]
         ref_end = ref_off + ref_len_subfind[sf_shi[icl]]
         ref_ids = ref_ids_subfind[ref_off : ref_end]
         
-        gate = st.Gate(sid, ref_ids)
-        ref_index = gate.in2()
-        ind_in_sf = np.nonzero(ref_index >= 0)[0]
-
-        full_mstar_sf[curr_cl] = np.sum(smass[ind_in_sf])
+        ref_index, ind_matched = hy.crossref.find_id_indices(
+            stars.ParticleIDs, ref_ids)
+        full_mstar_sf[curr_cl] = np.sum(stars.Mass[ind_matched])
 
         # Same for SFR (CA only)
-        readReg = ht.ReadRegion(snapdir, 0, [*cl_pos_sim, 0.03/conv_astro_pos])
-        
-        gpos = readReg.read_data("Coordinates", astro = True)
-        gsfr = readReg.read_data("StarFormationRate", astro = True)
-        gid = readReg.read_data("ParticleIDs", astro = False)
-         
+        gas = hy.ReadRegion(snapdir, 0, cl_pos, 0.03, exact=True)
+        gsfr = gas.StarFormationRate
+                 
         # Match to Cantor particles
-        if ref_eid_cantor[cantor_shi[icl]] < 0: set_trace()
-        ref_off = ref_off_cantor[cantor_shi[icl], 0]
-        ref_end = ref_end_cantor[ref_eid_cantor[cantor_shi[icl]], 0, 2]
+        if ref_eid_cantor[cshi] < 0: set_trace()
+        ref_off = ref_off_cantor[cshi, 0]
+        ref_end = ref_end_cantor[ref_eid_cantor[cshi], 0, 2]
         ref_ids = ref_ids_cantor[ref_off : ref_end]
 
-        gate = st.Gate(gid, ref_ids)
-        ref_index = gate.in2()
-        ind_in_ca = np.nonzero(ref_index >= 0)[0]
-
-        full_sfr_30kpc[curr_cl] = np.sum(gsfr[ind_in_ca])
-        
-       
+        ref_index, ind_matched = hy.crossref.find_id_indices(
+            gas.ParticleIDs, ref_ids)
+        full_sfr_30kpc[curr_cl] = np.sum(gas.StarFormationRate[ind_matched])
+               
         # Find time of last major halo merger
         for isnap_back in range(29, 0, -1):
-            
+        
+            # Find galaxies accreted to FOF in last interval
             ind_acc = np.nonzero(
                 (cenGal_all[mergelist[:, isnap_back], isnap_back] == icl) &
                 (cenGal_all[:, isnap_back-1] == np.arange(ngal)) &
-                (np.arange(ngal) != icl))[0]
-        
+                (np.arange(ngal) != icl)
+            )[0]
             if len(ind_acc) == 0: continue
 
-            m200_pre = np.max(m200_all[ind_acc, isnap_back-1])
+            m200_pre = np.max(m200_all[ind_acc, :isnap_back])
             m200_self = m200_all[icl, isnap_back-1]
-
-            print("Max merger in snap {:d}: {:.2f} vs. {:.2f}" 
-                  .format(isnap_back, m200_pre, m200_self))
+            print(f"Max merger in snap {isnap_back}: "
+                  f"{m200_pre:.2f} vs. {m200_self:.2f}") 
 
             if m200_pre >= m200_self + np.log10(1/10):
-                print("Major merger in snap {:d} (M200 = {:.2f} vs. {:.2f})"
-                      .format(isnap_back, m200_pre, m200_self))
+                print(f"Major merger in snap {isnap_back}!")
                 full_relaxTime[curr_cl] = snap_lbt[isnap_back] + (
-                    snap_lbt[isnap_back-1]-snap_lbt[isnap_back])/2
+                    snap_lbt[isnap_back-1] - snap_lbt[isnap_back]) / 2
+
+                # We can stop here, because we go backwards through snaps
                 break
 
         # Ends loop through snapshots for finding last major merger
     # Ends loop through clusters
 # Ends loop through simulations
 
-yb.write_hdf5(full_sim[:curr_cl], outloc, 'Sim', new = True)
-yb.write_hdf5(full_gal[:curr_cl], outloc, 'Galaxy')
-yb.write_hdf5(full_m200[:curr_cl], outloc, 'M200c')
-yb.write_hdf5(full_mstar_all[:curr_cl], outloc, 'MstarAll')
-yb.write_hdf5(full_mstar_ca[:curr_cl], outloc, 'MstarCantor')
-yb.write_hdf5(full_mstar_sf[:curr_cl], outloc, 'MstarSubfind')
-yb.write_hdf5(full_relaxTime[:curr_cl], outloc, 'RelaxTime')
-yb.write_hdf5(full_sfr_30kpc[:curr_cl], outloc, 'SFR_30kpc')
-yb.write_hdf5(full_r200[:curr_cl], outloc, 'R200c')
-
+hd.write_data(outloc, 'Sim', full_sim[:curr_cl], new=True)
+hd.write_data(outloc, 'Galaxy', full_gal[:curr_cl])
+hd.write_data(outloc, 'M200c', full_m200[:curr_cl])
+hd.write_data(outloc, 'MstarAll', full_mstar_all[:curr_cl])
+hd.write_data(outloc, 'MstarCantor', full_mstar_ca[:curr_cl])
+hd.write_data(outloc, 'MstarSubfind', full_mstar_sf[:curr_cl])
+hd.write_data(outloc, 'RelaxTime', full_relaxTime[:curr_cl])
+hd.write_data(outloc, 'SFR_30kpc', full_sfr_30kpc[:curr_cl])
+hd.write_data(outloc, 'R200c', full_r200[:curr_cl])
 
 print("Done!")
