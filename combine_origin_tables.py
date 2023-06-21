a2c14ba4d65486ece67c253d8b22ff43436139ae
Yannick Bahe <bahe@strw.leidenuniv.nl>
2023-06-21 14:11:48 +0200

    Updated combine_origin_tables.py

--- combine_origin_tables.py
+++ combine_origin_tables.py
@@ -2,80 +2,60 @@
 Small helper program to combine origin tables produced by different MPI tasks.
 
 Started 04-Jul-2019
+
+Updated 21-Jun-2023
 """
 
-import yb_utils as yb
+import h5py as h5
 import numpy as np
 import os
 from shutil import copyfile
 from pdb import set_trace
 
+# Combine entries across all simulations?
 combine_sims = True
+
+# Does the input catalogue contain parents?
 with_parents = False
 
-dataloc = ('/virgo/scratch/ybahe/HYDRANGEA/RESULTS/ICL/' +
-          #'OriginMasses_08Jul19_HostMhalo5_RootMsub24_HostRad7_firstroot')
-          # 'OriginMasses_2Aug19_HostCat_RootXTopGal_HostRad22_lastroot_noxd')
-          #'OriginMasses_1Aug19_HostCat_RootTopGal_HostRad22_lastroot_noxd')
-          #'OriginMasses_2Aug19_HostCat_RootMstar3_HostRad22_lastroot_withForgetfulParents_noxd')
-          #'OriginMasses_5Aug19_HostCat_RootMstar3_HostRad7_firstroot_XD')
-          #'OriginMasses_5Aug19_HostCat_RootMstar3_HostRad7_lastroot_tass28_'
-          #'noxd')
-          # 'OriginMasses_9Aug19_HostCat_RootXTopGal_HostRad22_firstroot_noxd')
-           'OriginMasses_20Sep19_HostCat_HostMass14_HostRad16_lastroot_DM')
-           #'OriginMasses_9Aug19_HostCat_RootXTopGal_HostRad22_lastroot_noxd')
-           #'OriginMasses_8Aug19_HostCat_HostRad7_lastroot_tass28')
-           #'OriginMasses_8Aug19_HostCat_HostRad7_RootMass26_firstroot_noxd')
-           #'OriginMasses_8Aug19_HostCat_HostRad7_RootMass6_firstroot_XD')
-           #'OriginMasses_8Aug19_HostCat_HostRad22_firstroot_noxd')
-           #'OriginMasses_5Aug19_HostCat_RootMstar6_HostRad7_firstroot_XDage')
+# File (prefix) of the files to combine
+dataloc = 'OriginMasses_Test'
 
-nfiles = 8
+# Number of files to combine
+nfiles = 1
 
-stars = False
-dm = True
+# --------------------------------------------------------------------------
 
-copyfile(dataloc + '.0.hdf5', dataloc + '.hdf5')
+outloc = dataloc + '.hdf5'
 
-if stars:
-    mbins_stars = yb.read_hdf5(dataloc + '.hdf5', 'BinnedMasses_Stars')
-if dm:
-    mbins_dm = yb.read_hdf5(dataloc + '.hdf5', 'BinnedMasses_DM')
+data = {}
+hdr_attrs = {}
+with h5.File(dataloc + '.0.hdf5', 'r') as f:
+    dsets = list(f.keys())
+    if 'Header' in dsets: dsets.remove('Header')
+    for key in dsets:
+        data[key] = f[key][...]
+    for key in f['Header'].attrs:
+        hdr_attrs[key] = f['Header'].attrs[key]
 
-if with_parents:
-    mbins_stars_par = yb.read_hdf5(dataloc + '.hdf5', 
-                                   'BinnedMasses_StarsParent')
+# Now copy in the content of the other files
+for ifile in range(1, nfiles):
+    with h5.File(dataloc + f'.{ifile}.hdf5', 'r') as f:
+        for dset in dsets:
+            data[dset] += f[dset][...]
 
-for ii in range(1, nfiles):
-    filename = dataloc + '.' + str(ii) + '.hdf5'
-
-    if stars:
-        mbins_stars_part = yb.read_hdf5(filename, 'BinnedMasses_Stars')
-        mbins_stars += mbins_stars_part
-    if dm:
-        mbins_dm_part = yb.read_hdf5(filename, 'BinnedMasses_DM')
-        mbins_dm += mbins_dm_part
-        
-
-    if with_parents:
-        mbins_starsPar_part = yb.read_hdf5(filename, 
-                                           'BinnedMasses_StarsParent')
-        mbins_stars_par += mbins_starsPar_part
-
+# Done reading and stacking all the data. If desired, collapse output array
+# over the simulation axis (0), since it is redundant.
 if combine_sims:
-    if stars:
-        mbins_stars = np.sum(mbins_stars, axis = 0)
-    if dm:
-        mbins_dm = np.sum(mbins_dm, axis = 0)
-    if with_parents:
-        mbins_stars_par = np.sum(mbins_stars_par, axis = 0)
+    for dset in dsets:
+        data[dset] = np.sum(data[dset], axis=0)
 
-if stars:
-    yb.write_hdf5(mbins_stars, dataloc + '.hdf5', 'BinnedMasses_Stars')
-if dm:
-    yb.write_hdf5(mbins_dm, dataloc + '.hdf5', 'BinnedMasses_DM')
-if with_parents:
-    yb.write_hdf5(mbins_stars_par, dataloc + '.hdf5', 
-                  'BinnedMasses_StarsParent')
+# Write output
+with h5.File(dataloc + '.hdf5', 'w') as f:
+    for dset in dsets:
+        f[dset] = data[dset]
+    for key in hdr_attrs:
+        f['Header'].attrs[key] = hdr_attrs[key]
+
 
 print("Done!")
